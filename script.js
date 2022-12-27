7e85d8db5f79d5fd0138e14bbfa344e84c57ed84
YASH PRATAP SINGH <65596695+11yashpratapsingh@users.noreply.github.com>
2022-12-28 00:54:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "4c34362f92mshec82f03f5e82c26p1e1623jsn082ccef46733",
-    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    "X-RapidAPI-Key": "",
+    "X-RapidAPI-Host": "",
   },
 };
 
